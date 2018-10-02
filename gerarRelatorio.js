3323df15a1042ae73761411758f33e1c99edb047
Lucas Neuhaus <lucas.neuhaus93@gmail.com>
2018-10-02 17:19:10 -0300

    Added a lot of things

--- gerarRelatorio.js
+++ gerarRelatorio.js
@@ -3,38 +3,45 @@ function gerarRelatorio() {
   
   cloneTemplate();
   
+  ss.getSheetByName("Loading").showSheet().activate();
+  
   var reportSheet = ss.getSheetByName('Relatório');
   var tasksSheet = ss.getSheetByName('Tarefas');
   
+  reportSheet.hideSheet();  
   
+  // tasksSheet.activate();
   var dataRange = tasksSheet.getDataRange();
   var rangeValues = dataRange.getValues();
   var numberOfRows = dataRange.getLastRow();
   
   for(i = 2; i < numberOfRows; i++){
     
+    var progression = (i - 2) / (numberOfRows - 2);
+    ss.getSheetByName("Loading").getRange('B1').setValue(progression);
+    
     var situacao = rangeValues[i][3];
     
     switch(situacao){
       case "Metas":
-        Logger.log(i + ": Metas");
         copyPasteTasks(i+1, 1);
         break;
         
       case "Concluídas":
-        Logger.log(i + ": Concluídas");
         copyPasteTasks(i+1, 2);
         break;
         
       case "À espera de execução":
-        Logger.log(i + ": À espera de execução");
         copyPasteTasks(i+1, 3);
     }
 
   }
   
-  ss.getRange('D:D').activate();
-  ss.getActiveSheet().deleteColumns(ss.getActiveRange().getColumn(), ss.getActiveRange().getNumColumns());
+  reportSheet.deleteColumn(4);
+  
+  reportSheet.showSheet();
+  SpreadsheetApp.getActive().setActiveSheet(reportSheet);
+  SpreadsheetApp.getActive().getSheetByName("Loading").hideSheet();
   
 }
 
@@ -43,42 +50,40 @@ function copyPasteTasks(rowIndex, taskCondition){
   var reportSheet = ss.getSheetByName('Relatório');
   
   
-  reportSheet.getRange('D1').activate();
+  var aux = reportSheet.getRange('D1');
   
   for (j = 0; j < taskCondition; j++){
-    ss.getCurrentCell().getNextDataCell(SpreadsheetApp.Direction.DOWN).activate();
+    aux = aux.getNextDataCell(SpreadsheetApp.Direction.DOWN);
   }
   
-  ss.getActiveSheet().insertRowsAfter(ss.getActiveRange().getLastRow(), 1);
-  ss.getActiveRange().offset(1, 0).activate();
-  var lastRow = ss.getActiveRange().getLastRow();
-  Logger.log(lastRow);
+  reportSheet.insertRowAfter(aux.getLastRow());
+  aux = aux.offset(1,0);
+  var lastRow = aux.getLastRow();
   
-  ss.getActiveRange().offset(-1, 0).moveTo(ss.getActiveRange());
-  ss.getCurrentCell().getNextDataCell(SpreadsheetApp.Direction.PREVIOUS).activate();
-  ss.getRange('Tarefas!A' + rowIndex + ':C' + rowIndex).copyTo(ss.getActiveRange(), SpreadsheetApp.CopyPasteType.PASTE_VALUES, false);
+  aux.offset(-1, 0).moveTo(aux);
+  aux = aux.getNextDataCell(SpreadsheetApp.Direction.PREVIOUS);
+  ss.getRange('Tarefas!A' + rowIndex + ':C' + rowIndex).copyTo(aux, SpreadsheetApp.CopyPasteType.PASTE_VALUES, false);
   
-  ss.getActiveSheet().setRowHeight(lastRow, 80);
-  ss.getRange('A' + lastRow + ':C' + lastRow).activate();
-  ss.getActiveRangeList().setVerticalAlignment('middle')
+  reportSheet.setRowHeight(lastRow, 80);
+  var taskRow = reportSheet.getRange('A' + lastRow + ':C' + lastRow);
+  taskRow.setVerticalAlignment('middle')
   .setWrapStrategy(SpreadsheetApp.WrapStrategy.WRAP);
+  
+  SpreadsheetApp.flush();
   
 }
 
 function cloneTemplate(){
   var name = "Relatório";
   var ss = SpreadsheetApp.getActive();
-  var template = ss.getSheetByName("Template").copyTo(ss);
+  var sheet = ss.getSheetByName("Template").copyTo(ss);
   
   /* Before cloning the sheet, delete any previous copy */
   var old = ss.getSheetByName(name);
   if (old) ss.deleteSheet(old); // or old.setName(new Name);
   
   SpreadsheetApp.flush(); // Utilities.sleep(2000);
-  template.setName(name);
-
-  /* Make the new sheet active */
-  ss.setActiveSheet(template);
+  sheet.setName(name);
   
 }
 
